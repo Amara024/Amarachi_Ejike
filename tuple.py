a5aea85211f1172b228ab665c5b60aff30e77a03
amara024 <105262056+Amara024@users.noreply.github.com>
2023-02-04 01:37:19 +0100

    Add files via upload

---
+++ tuple.py
@@ -0,0 +1,24 @@
+tuple 
+
+#count
+age=(12,13,16,17,18,12)
+x=age.count(12)
+print(x)
+
+#indexig
+y=age.index(16)
+print(x)
+
+#negative indexing
+print(age[-4])
+
+#slicing
+print(age[2:5])
+
+#true or false
+print(40 in age) #false
+print(16 in age) #true
+print('16' in age) #false because'16' is a string
+
+
+
